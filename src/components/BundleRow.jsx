abf506f1c474e148be1f83c7393bf751f67172db
nevets-steven <nevets322@gmail.com>
2025-11-19 19:35:52 -0500

    fixed error for Linux env

--- src/components/BundleRow.jsx
+++ src/components/BundleRow.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import '../BundleRow.css'
+import './BundleRow.css'
 
 export default function BundleRow({title, image1, image2, price, onQuantityChange}){
     const [qty, setQty] = useState(0);
